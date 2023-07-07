e571d4a8fc69d446c1c04f4667dcaf9ced419236
Maik Knof <knofm@hs-weingarten.de>
2023-07-07 17:09:39 +0200

    add ros noetic wrapper

--- ros/ros_objet_map_server/src/ros_map_server/main.py
+++ ros/ros_objet_map_server/src/ros_map_server/main.py
@@ -1,36 +1,79 @@
 #!/usr/bin/env python3
 
 import rospy
+import tf2_ros
+from geometry_msgs.msg import TransformStamped
+from visualization_msgs.msg import MarkerArray
 
-from ros_map_server_interfaces.msg import Example
-from map_server import example_function
+from object_map_server.interfaces import load_objects, load_frames, Frame, Object
+from ros_object_map_server.conversion import objects2markerarray, pose2rospose
 
-class MapServer():
-    def __init__(self):
-        rospy.init_node("ros_map_server", anonymous=True)
-        rospy.loginfo("Starting")
+OBJECT_TOPIC = "/object_map_server/objects"
+RATE = 1.0
+
+class ObjectMapServer():
+    def __init__(self, object_path: str, frame_path: str):
+        rospy.init_node("object_map_server", anonymous=True)
+        
+        rospy.loginfo("Loading objects")
+        self.objects = load_objects(object_path)
+        
+        rospy.loginfo("Loading frames")
+        self.root_frame = load_frames(frame_path)
         
         # Publisher
-        #self.pub = rospy.Publisher(TOPIC, MSG, queue_size=10)
+        self.pub = rospy.Publisher(OBJECT_TOPIC, MarkerArray, queue_size=10)
 
-        # Subscriber
-        #rospy.Subscriber(TOPIC, MSG, CALLBACK)
+        # Tf broadcaster
+        self.br = tf2_ros.TransformBroadcaster()
 
-        #Execute main loop
-        rate = rospy.Rate(1)
+        self.rate = rospy.Rate(RATE)
         while not rospy.is_shutdown():
-            self.loop()
-            rate.sleep()
+            self.publish_objects()
+            self.publish_tf_recursiv(self.root_frame)
+            self.rate.sleep()
 
-    def loop(self):
+    def publish_objects(self):
+        markers = objects2markerarray(self.objects)
+        self.pub.publish(markers)
+
+    def publish_tf(self, frame_id, child_frame_id, pose):
         '''
-        main loop
+        publish tf
         '''
-        rospy.loginfo(Example())
-        example_function()
+        t = TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = frame_id
+        t.child_frame_id = child_frame_id
+        t.transform.translation.x = pose.position.x
+        t.transform.translation.y = pose.position.y
+        t.transform.translation.z = pose.position.z
+        t.transform.rotation.x = pose.orientation.x
+        t.transform.rotation.y = pose.orientation.y
+        t.transform.rotation.z = pose.orientation.z
+        t.transform.rotation.w = pose.orientation.w
+        self.br.sendTransform(t)
+
+    def publish_tf_recursiv(self, frame):
+        '''
+        publish tf recursivly
+        '''
+        #if frame has parent, publish tf
+        if isinstance(frame.parent, Frame):
+            self.publish_tf(frame.parent.name, frame.name, pose2rospose(frame.pose))
+
+        #recursivly call this function for all children
+        for child in frame.children:
+            self.publish_tf_recursiv(child)
 
 
 
 if __name__ == '__main__':
-    MapServer()
+    import argparse
+    parser = argparse.ArgumentParser(description='Object Map Server')
+    parser.add_argument('--object_path', type=str, help='path to objects')
+    parser.add_argument('--frame_path', type=str, help='path to frames')
+    args = parser.parse_args()
+
+    ObjectMapServer(args.object_path, args.frame_path)
 
