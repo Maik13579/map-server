35a9e92de3c56d429c026c0682b4190904999fb0
Maik Knof <knofm@hs-weingarten.de>
2023-07-22 14:56:47 +0200

    add service to generate grid map for ros1

--- ros/ros_object_map_server/src/ros_object_map_server/main.py
+++ ros/ros_object_map_server/src/ros_object_map_server/main.py
@@ -2,11 +2,15 @@
 
 import rospy
 import tf2_ros
-from geometry_msgs.msg import TransformStamped, Pose
+from geometry_msgs.msg import TransformStamped, Pose, PoseStamped
 from visualization_msgs.msg import MarkerArray
+from nav_msgs.msg import OccupancyGrid
 
-from object_map_server import load_objects, load_frames, Frame, save_objects, save_frames, object_as_sdf
-from conversion import objects2markerarray, pose2rospose, compose_poses
+from tf2_geometry_msgs import do_transform_pose
+
+from object_map_server import load_objects, load_frames, Frame, save_objects, save_frames, \
+object_as_sdf, cube_plane_intersection, cylinder_plane_intersection, intersection_to_grid_map
+from conversion import objects2markerarray, pose2rospose, compose_poses, object2markerarray, create_occupancy_grid
 from interactive_object_server import InteractiveObjectServer
 
 from ros_object_map_server_interfaces.srv import Save, Load, SaveResponse, LoadResponse
@@ -15,6 +19,7 @@ from std_srvs.srv import Trigger, TriggerResponse
 from gazebo_msgs.srv import SpawnModel
 
 OBJECT_TOPIC = "/object_map_server/objects"
+MAP_TOPIC = "/object_map_server/map"
 RATE = 5
 
 class ObjectMapServer():
@@ -25,6 +30,7 @@ class ObjectMapServer():
         self.save_service = rospy.Service('object_map_server/save', Save, self.service_save)
         self.load_service = rospy.Service('object_map_server/load', Load, self.service_load)
         self.spawn_gazebo_service = rospy.Service('object_map_server/spawn_gazebo', Trigger, self.service_spawn_gazebo)
+        self.generate_occupancy_grid_service = rospy.Service('object_map_server/generate_occupancy_grid', Trigger, self.service_generate_occupancy_grid)
         
         rospy.loginfo("Loading objects")
         self.object_path = object_path
@@ -40,16 +46,40 @@ class ObjectMapServer():
         
         # Publisher
         self.pub = rospy.Publisher(OBJECT_TOPIC, MarkerArray, queue_size=10)
+        self.pub_grid = rospy.Publisher(MAP_TOPIC, OccupancyGrid, queue_size=10)
 
         # Tf broadcaster
         self.br = tf2_ros.TransformBroadcaster()
 
+        # Tf listener
+        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+
+
         self.rate = rospy.Rate(RATE)
+        self.occupancy_grid = None
+
         while not rospy.is_shutdown():
             self.publish_objects()
             self.publish_tf_recursiv(self.root_frame)
             self.server.main_loop()
+            
+            if self.occupancy_grid is not None:
+                self.pub_grid.publish(self.occupancy_grid)
             self.rate.sleep()
+
+    def service_generate_occupancy_grid(self, request):
+        response = TriggerResponse()
+        rospy.loginfo('Generating occupancy grid...')
+        try:
+            self.occupancy_grid = self.generate_occupancy_grid(self.root_frame_name, 10.0, 10.0, 0.04)
+        except Exception as e:
+            response.success = False
+            response.message = str(e)
+            return response
+        response.success = True
+        response.message = "Generated occupancy grid in frame " + self.root_frame_name + " with size 10x10m and resolution 0.04m"
+        return response
 
     def service_spawn_gazebo(self, request):
         response = TriggerResponse()
@@ -161,6 +191,64 @@ class ObjectMapServer():
         for child in frame.children:
             self.publish_tf_recursiv(child)
 
+    def generate_occupancy_grid(self, frame, width, height, resolution):
+        '''
+        generate occupancy grid
+        '''
+        all_intersections = []
+        for obj in self.objects:
+            #transform object to [frame]
+
+            #get transform to target frame
+            transform = self.tf_buffer.lookup_transform(frame,
+                                       obj.name,
+                                       rospy.Time.now(),
+                                       rospy.Duration(10.0))
+            #convert to marker array
+            obj_markers = object2markerarray(obj)
+
+            #transform markers to frame
+            transformed_markers = []
+            for marker in obj_markers.markers:
+                posestamped = PoseStamped()
+                posestamped.header.stamp = rospy.Time.now()
+                posestamped.header.frame_id = obj.name
+                posestamped.pose = marker.pose
+                marker.pose = do_transform_pose(posestamped, transform)
+                transformed_markers.append(marker)
+
+            #compute intersections
+            for marker in transformed_markers:
+                if marker.type == 1: #cube
+                    if marker.text == 'bb': #skip bounding boxes
+                        continue
+                    intersection = cube_plane_intersection(
+                        (marker.pose.pose.position.x, marker.pose.pose.position.y, marker.pose.pose.position.z),
+                        (marker.pose.pose.orientation.x, marker.pose.pose.orientation.y, marker.pose.pose.orientation.z, marker.pose.pose.orientation.w),
+                        (marker.scale.x, marker.scale.y, marker.scale.z),
+                        (0.0, 0.0, 1.0),
+                        (0.0, 0.0, 0.1) #height of laser scanner from PAL tiago rougly is 10 cm
+                    )
+                    if len(intersection) > 0:
+                        all_intersections.append(intersection)
+
+                elif marker.type == 3: #cylinder
+                    intersection = cylinder_plane_intersection(
+                        (marker.pose.pose.position.x, marker.pose.pose.position.y, marker.pose.pose.position.z),
+                        (marker.pose.pose.orientation.x, marker.pose.pose.orientation.y, marker.pose.pose.orientation.z, marker.pose.pose.orientation.w),
+                        marker.scale.z,
+                        marker.scale.x/2,
+                        (0.0, 0.0, 1.0),
+                        (0.0, 0.0, 0.1) #height of laser scanner from PAL tiago rougly is 10 cm
+                    )
+                    if len(intersection) > 0:
+                        all_intersections.append(intersection)
+        #convert to grid map
+        image, w, h = intersection_to_grid_map(all_intersections, (width, height), resolution)
+        occupancy_grid = create_occupancy_grid(resolution, frame, image, w, h)
+        occupancy_grid.header.stamp = rospy.Time.now()
+        occupancy_grid.info.map_load_time = rospy.Time.now()
+        return occupancy_grid
 
 
 if __name__ == '__main__':
